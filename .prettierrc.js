dd3f19bdad0a2e08be33c2849df2c0ac4390733b
lzdFeiFei <2802486551@qq.com>
2022-05-24 11:48:19 +0800

    prettier 配置

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: zhengda.lv
+ * @Date: 2021-07-08 13:35:31
+ * @LastEditTime: 2021-11-30 14:14:23
+ * @LastEditors: zhengda.lv
+ * @Description:
+ */
+module.exports = {
+  tabWidth: 2,
+  singleQuote: true, // 使用单引号
+  semi: false, // 句尾不使用分号
+  printWidth: 80,
+}
