33307f405c52d15c4eb850ca769fba144ad69924
lzdFeiFei <2802486551@qq.com>
2022-05-27 14:40:53 +0800

    feat: 图片懒加载 v-lazy

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import lazyPlugin from 'vue3-lazy'
 
 import '@/assets/scss/index.scss'
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+  .use(router)
+  .use(lazyPlugin, {
+    loading: require('@/assets/images/default.png'),
+  })
+  .mount('#app')
