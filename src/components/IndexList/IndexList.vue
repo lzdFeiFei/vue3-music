8b03a9c27f19834bbe7c25f6025f1cae5a88c42a
lzdFeiFei <2802486551@qq.com>
2022-05-30 14:18:47 +0800

    feat: 获取歌手接口

---
+++ src/components/IndexList/IndexList.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <Scroll
+      class="index-list"
+      :probe-type="3"
+      @scroll="onScroll"
+      ref="scrollRef"
+    >
+      <ul ref="groupRef">
+        <li class="group" v-for="group in data" :key="group.title">
+          <h2 class="title">{{ group.title }}</h2>
+          <ul>
+            <li
+              v-for="item in group.list"
+              :key="item.id"
+              class="item"
+              @click="onItemClick(item)"
+            >
+              <img class="avatar" v-lazy="item.pic" /><span class="name">{{
+                item.name
+              }}</span>
+            </li>
+          </ul>
+        </li>
+      </ul>
+      <div class="fixed" v-show="fixedTitle" :style="fixedStyle">
+        <div class="fixed-title">{{ fixedTitle }}</div>
+      </div>
+    </Scroll>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Scroll from './Scroll/Scroll.vue'
+</script>
+
+<style scoped></style>
--- src/views/singer/index.vue
+++ src/views/singer/index.vue
@@ -2,6 +2,14 @@
   <div>歌手</div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+import { getSingerList } from '@/service/singer'
+const singers = ref<any>([])
+getSingerList().then((res: any) => {
+  console.log(res)
+  singers.value = res
+})
+</script>
 
 <style lang="scss" scoped></style>
